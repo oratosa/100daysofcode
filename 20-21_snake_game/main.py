59f0852df8b08f753678f00d353e94e303b87907
oratosa <c121034t@gmail.com>
2024-07-10 22:03:07 -0700

    Updated the program to memorize the high score was taken by the last time.

--- 20-21_snake_game/main.py
+++ 20-21_snake_game/main.py
@@ -41,14 +41,14 @@ while game_is_on:
         or snake.head.ycor() > 280
         or snake.head.ycor() < -280
     ):
-        scoreboard.game_over()
-        game_is_on = False
+        scoreboard.reset()
+        snake.reset()
 
     # detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            scoreboard.game_over()
-            game_is_on = False
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
--- 20-21_snake_game/scoreboard.py
+++ 20-21_snake_game/scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import os
 
 ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
@@ -12,13 +13,39 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(x=0, y=270)
         self.score = 0
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.high_score = self.read_high_score_from_file()
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(x=0, y=0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(x=0, y=0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_high_score_on_file(self.high_score)
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(
+            f"Score: {self.score}, High Score: {self.high_score}",
+            align=ALIGNMENT,
+            font=FONT,
+        )
+
+    def read_high_score_from_file(self):
+        dir = os.path.dirname(__file__)
+        with open(f"{dir}/data.txt", "r") as f:
+            previous_high_score = int(f.read())
+            return previous_high_score
+
+    def write_high_score_on_file(self, score):
+        dir = os.path.dirname(__file__)
+        with open(f"{dir}/data.txt", "w") as f:
+            f.write(str(score))
